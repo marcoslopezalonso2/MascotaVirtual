82bd68ee39743e4b320145ba4bf6ced1a4e56bbb
marcoslopezalonso2 <marcoslopezalonso22@gmail.com>
2022-12-25 23:26:02 +0100

    Introduje los atributos de la clase RatoncitoFiuFiu

---
+++ MASCOTA/src/mascota/RatoncitoFiuFiu.java
@@ -0,0 +1,57 @@
+package mascota;
+
+public class RatoncitoFiuFiu {
+     private String nombre;
+     private int edad;
+     private double peso;
+     private int sensacionDeHambre;
+     private int nivelDeSuciedad;
+     private int nivelDeSalud;
+     private int nivelDeEnergia;
+
+    public RatoncitoFiuFiu(String nombre, int i, int i1, int i2, int i3, int i4) {
+        // Un objeto mascota.RatoncitoFiuFiu debería informar cuando nace...
+    }
+
+
+    public String estadisticas() {
+        return null;
+    }
+
+    public void limpiar(int i) {
+    }
+
+    public int queTramoEdad() {
+        return 0;
+    }
+
+    public boolean estasDormido() {
+        return false;
+    }
+
+    public boolean estasEnfermo() {
+        return false;
+    }
+
+    public boolean estasSucio() {
+        return false;
+    }
+
+    public boolean estasMuerto() {
+        return false;
+    }
+
+    public void envejecer(int i) {
+    }
+
+    public boolean tienesQuejas() {
+        return false;
+    }
+
+    public void alimentar(int b) {
+
+    }
+
+    public void curar(int i) {
+    }
+}
